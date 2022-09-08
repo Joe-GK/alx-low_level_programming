0ce25432d47426848524ff1aa8a747be44532042
Joe-GK <joel.kabz@gmail.com>
2022-09-08 15:01:48 +0000

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main will display string in funtion (put)
+*string ="Programming is like building a multilingual puzzle"
+* the  return=0
+*/
+int main(void)
+{
+puts( "\" "Programming is like building a multilingual puzzle");
+return (0);
+}
