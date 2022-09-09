06c5ad1e75e3639a627e7e05b4f530ab591f8eab
Joe-GK <joel.kabz@gmail.com>
2022-09-09 07:15:10 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 /**
-*main will display string in funtion (put)
-*string ="Programming is like building a multilingual puzzle"
-* the  return=0
+*main - It will print out a string using the put function
+*
+* Description: using the main function 
+*The program will out put "Programming is like building a multilingual puzzle"
+* Return:0
 */
 int main(void)
 {
-puts( "\" "Programming is like building a multilingual puzzle");
-return (0);
+      puts("\"Programming is like building a multilingual puzzle");
+      return (0);
 }
+^c
